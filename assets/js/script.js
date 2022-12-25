6259310e24938a51d13da01b610d96c935216b7b
AwsSG <aws.sabah.gheni@gmail.com>
2022-12-25 04:54:58 +0000

    added basic sprite movement

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+// Set the interval for image change (in milliseconds)
+const interval = 100;
+
+// Select the image element
+const sprite = document.getElementById('sprite');
+
+// Set the initial image source
+let currentImage = 0;
+const imageSources = [
+  'assets/images/1.png',
+  'assets/images/2.png',
+  'assets/images/3.png',
+  'assets/images/4.png',
+  'assets/images/5.png'
+
+];
+sprite.src = imageSources[currentImage];
+
+// Change the image every interval
+setInterval(() => {
+  // Increment the current image
+  currentImage = (currentImage + 1) % imageSources.length;
+
+  // Update the image source
+  sprite.src = imageSources[currentImage];
+}, interval);
